54ddc0c8bf9e1e7513361233380a0299621d815e
Jochen Stärk <jstaerk@usegroup.de Jochen Staerk>
2017-07-31 19:39:02 +0200

    using maven for assetsbundle

--- src/main/java/zox/HelloWorldApplication.java
+++ src/main/java/zox/HelloWorldApplication.java
@@ -1,8 +1,10 @@
 package zox;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -17,6 +19,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
     		bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+    		 bootstrap.addBundle(new ViewBundle<HelloWorldConfiguration>());
     }
 
     @Override
